13245ae236d03383d8b7dd9627bbc71be68fbac8
Cbailey371 <cbaileyh26@yahoo.com>
2026-01-30 12:46:21 -0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,8 @@
 
     <!-- Manifest PWA -->
     <link rel="manifest" href="/manifest.json">
-    <link rel="icon" type="image/x-icon" href="{{ $settings['hotel_favicon'] ?? '/favicon.ico' }}">
+    <link rel="icon" type="image/x-icon"
+        href="{{ $settings['hotel_favicon'] ?? $settings['hotel_logo'] ?? '/favicon.ico' }}">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap"
